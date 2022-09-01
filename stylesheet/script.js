8402743c1efff1b2155f6dd3face04d4cd6de8b5
hirose <mayu122536@gmail.com>
2022-09-01 12:03:05 +0900

    右から出てくるメニューの開閉時にアイコンの色が変わるように変更しました。

--- stylesheet/script.js
+++ stylesheet/script.js
@@ -1,5 +1,14 @@
   const icon = document.querySelector('.icon');
   const nav = document.querySelector('nav');
   icon.addEventListener('click', () => {
-  nav.classList.toggle('open-menu')
+    nav.classList.toggle('open-menu')
+    icon.classList.toggle('open_menu_icon')
+    if(nav.classList.contains('open-menu')){
+      icon.classList.remove('icon')
+      icon.classList.add('open_menu_icon')
+    }else if(nav.classList.contains('open-menu')==false){
+    icon.classList.remove('open_menu_icon')
+    icon.classList.add('icon')
+    }
 });
+
